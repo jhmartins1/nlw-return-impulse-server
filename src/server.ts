04eeb76ba6ac10f7cdad1e65b7c992fa8133c2d1
João Heitor <jhmartinsrdo@gmail.com>
2022-05-11 22:45:12 -0300

    Add port env variable

--- src/server.ts
+++ src/server.ts
@@ -9,4 +9,4 @@ app.use(express.json());
 app.use(routes);
 
 
-app.listen(3333, () => console.log('Server running on port 3333!'));
+app.listen(process.env.PORT || 3333, () => console.log(`Server running on port ${PORT || 3333}`));
