9d63e4580fdbbcca9aa39ceb1cbda22c1b623259
João Heitor <jhmartinsrdo@gmail.com>
2022-05-11 23:53:34 -0300

    ajust

--- src/server.ts
+++ src/server.ts
@@ -9,4 +9,4 @@ app.use(express.json());
 app.use(routes);
 
 
-app.listen(process.env.PORT || 3333, () => console.log(`Server is running}`));
+app.listen(process.env.PORT || 3333, () => console.log(`Server is running`));
